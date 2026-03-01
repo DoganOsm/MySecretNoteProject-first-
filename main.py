45e2211b8ef13e5489a87bde23d6217ce20df984
Osman Doğan <hamuskun@gmail.com>
2026-03-01 23:24:26 +0300

    Basic İnterface Added

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+window = Tk()
+window.title("Secret Note")
+window.config(background="gray")
+
+enter_title = Label(window, text="Secret Note", fg="black", bg="gray")
+enter_title_entry = Entry(width=25)
+enter_title.pack()
+enter_title_entry.pack()
+
+user_mesage_label = Label(text="Enter your secret mesage", fg="black", bg="gray")
+user_mesage_label.pack()
+user_mesage_title = Text(height=20, width=40)
+user_mesage_title.pack()
+
+user_enter_key = Label(text="Enter your secret key", fg="black", bg="gray")
+user_enter_key.pack()
+user_enter_key_entry = Entry(width=25)
+user_enter_key_entry.pack()
+
+save_encrypt_button = Button(text="Save & Encrypted ", fg="black", bg="light gray",)
+save_encrypt_button.config(padx=20)
+save_encrypt_button.pack(pady=10)
+
+decrypt_button = Button(text="Decrypt", fg="black", bg="light gray",)
+decrypt_button.pack()
+
+window.mainloop()
