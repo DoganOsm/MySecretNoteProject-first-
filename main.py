03ddcf9d59f8666976a7406190ff09b217e1e828
Osman Doğan <hamuskun@gmail.com>
2026-03-02 01:27:56 +0300

    İmage Added

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from tkinter import *
+from PIL import ImageTk, Image
+
 
 window = Tk()
 window.title("Secret Note")
 window.config(background="gray")
+window.minsize(width=400, height=300)
+
+
+my_image = Image.open("output.png",)
+my_image_tk = ImageTk.PhotoImage(my_image)
+image_label = Label(window, image=my_image_tk)
+image_label.pack()
+image_label.config(bg="gray")
+
+
+
+
 
 enter_title = Label(window, text="Secret Note", fg="black", bg="gray")
 enter_title_entry = Entry(width=25)
@@ -25,5 +39,7 @@ save_encrypt_button.pack(pady=10)
 
 decrypt_button = Button(text="Decrypt", fg="black", bg="light gray",)
 decrypt_button.pack()
+
+
 
 window.mainloop()
