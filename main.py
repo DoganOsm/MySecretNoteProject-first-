76bcbafbc6fa66753eaf339b53bb1a724a05c07c
Osman Doğan <hamuskun@gmail.com>
2026-03-04 05:06:11 +0300

    First Fonction Added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 from PIL import ImageTk, Image
 
 
@@ -18,24 +20,38 @@ image_label.config(bg="gray")
 
 
 
-enter_title = Label(window, text="Secret Note", fg="black", bg="gray")
+enter_title = Label(window, text="Enter your title", fg="black", bg="gray")
 enter_title_entry = Entry(width=25)
 enter_title.pack()
 enter_title_entry.pack()
 
-user_mesage_label = Label(text="Enter your secret mesage", fg="black", bg="gray")
+user_mesage_label = Label(text="Enter Your Secret Mesage", fg="black", bg="gray")
 user_mesage_label.pack()
 user_mesage_title = Text(height=20, width=40)
 user_mesage_title.pack()
 
-user_enter_key = Label(text="Enter your secret key", fg="black", bg="gray")
+user_enter_key = Label(text="Enter Your Secret Key", fg="black", bg="gray")
 user_enter_key.pack()
 user_enter_key_entry = Entry(width=25)
 user_enter_key_entry.pack()
 
-save_encrypt_button = Button(text="Save & Encrypted ", fg="black", bg="light gray",)
+
+def new_create_file():
+    with open("secret.txt", "w") as file:
+        file.write(f"{enter_title_entry.get()}:")
+        file.write("\n",)
+        file.write("\t")
+        file.write(user_mesage_title.get("1.0",END))
+
+
+
+
+
+save_encrypt_button = Button(text="Save & Encrypted ", fg="black", bg="light gray",command=new_create_file)
 save_encrypt_button.config(padx=20)
 save_encrypt_button.pack(pady=10)
+
+
 
 decrypt_button = Button(text="Decrypt", fg="black", bg="light gray",)
 decrypt_button.pack()
